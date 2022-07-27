1347b623eeb4eca7853c2d86e28f735b24b384ec
Hugo Mafra <hugorc10@gmail.com>
2022-07-27 02:39:30 -0300

    :bug: fix: corrigindo bug na pagina resumo

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -9,8 +9,6 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = 'https://raw.githubusercontent.com/Hugorc10/portfolio-hugo/dev/src/Assets/cv.pdf';
-
 function Resume() {
   const [width, setWidth] = useState(1200);
 
@@ -23,21 +21,10 @@ function Resume() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: '250px' }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.3 : 0.6} />
           </Document>
         </Row>
 
